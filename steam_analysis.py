fd62ad1d89fd20c0a9f0906ed3afaf1e9c73d2bf
RuizJul <julrurojas@espol.edu.ec>
2025-10-06 19:51:41 -0500

    Agregar releaseYear para ver tendencias con el tiempo

--- steam_analysis.py
+++ steam_analysis.py
@@ -1,12 +1,28 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 df= pd.read_csv("steam_games_last10years.csv")
 
-# Configurar pandas para mostrar todo
-pd.set_option('display.max_columns', None)   # Mostrar todas las columnas
-pd.set_option('display.max_rows', 30)       # Mostrar 30 filas
-pd.set_option('display.width', None)        # Evita cortes por ancho de pantalla
-pd.set_option('display.max_colwidth', None) # Mostrar contenido largo completo
-
 # Imprimir las primeras 30 filas
 print(df.head(30))
+
+# Filtrar juegos difíciles
+df_dificiles = df[df["Dificultad"] == 1]
+
+
+#Calcular promedio
+promedio_por_anio = (
+    df_dificiles.groupby("ReleaseYear")["AverageForever"]
+    .mean()
+    .reset_index()
+    .sort_values("ReleaseYear")
+)
+
+plt.figure(figsize=(10,6))
+plt.plot(promedio_por_anio["ReleaseYear"], promedio_por_anio["AverageForever"], marker="o", linewidth=2)
+plt.title("Promedio de tiempo jugado en juegos difíciles por año", fontsize=14)
+plt.xlabel("Año de lanzamiento")
+plt.ylabel("Tiempo jugado promedio (minutos)")
+plt.grid(True, linestyle="--", alpha=0.6)
+plt.show()
